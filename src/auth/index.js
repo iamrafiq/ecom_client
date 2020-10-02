693ad0a40e3660474a81220953fe1ee3ddcff3d5
Your Name <you@example.com>
2020-10-03 00:36:23 +0600

    user signup message for success and failed

---
+++ src/auth/index.js
@@ -0,0 +1,21 @@
+
+import { API } from "../config";
+
+
+export const signup = (user) => {
+    console.log(user);
+    return fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((responce) => {
+        return responce.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,65 +1,96 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Layout from "../core/Layout";
-import { API } from "../config";
+import {Link} from "react-router-dom";
+import { signup } from "../auth/index";
+
 const Signup = () => {
   const [values, setValues] = useState({
-    name:'',
-    email:'',
-    password:'',
-    error:'',
-    success: false
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
 
-  })
-
-  const {name, email, password} = values;
-  const signup = (user)=>{
-    console.log(user);
-    fetch(`${API}/signup`,{
-      method:'POST',
-      headers:{
-        Accept:'application/json',
-        "Content-Type":"application/json"
-      },
-      body:JSON.stringify(user)
-    })
-    .then(responce=>{
-      return responce.json();
-    })
-    .catch(err=>{
-      console.log(err)
-    })
-  }
-  const handleChange = field => {
-    return event => {
-      setValues({ ...values, error:false, [field]:event.target.value})
-    }
-  }
-  const clickSubmit = (event)=>{
+  const { name, email, password, success, error } = values;
+ 
+  const handleChange = (field) => {
+    return (event) => {
+      setValues({ ...values, error: false, [field]: event.target.value });
+    };
+  };
+  const clickSubmit = (event) => {
     event.preventDefault();
-    signup({name, email, password});
-  }
-  const signUpFrom = ()=>(
+    setValues({...values, error:false})
+    signup({ name, email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({ ...values, name: "", email: "", error: "", success: true });
+      }
+    });
+  };
+  const signUpFrom = () => (
     <form action="">
       <div className="form-group">
         <label className="text-muted">Name</label>
-        <input onChange={handleChange('name')} type="text" className="form-control"/>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control"
+          value={name}
+        />
       </div>
       <div className="form-group">
         <label className="text-muted">Email</label>
-        <input onChange={handleChange('email')} type="email" className="form-control"/>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
       </div>
       <div className="form-group">
         <label className="text-muted">Password</label>
-        <input onChange={handleChange('password')} type="password" className="form-control"/>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
       </div>
-      <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      New account is created. Please <Link to="/signin"> signin </Link>.
+    </div>
+  );
   return (
-    <Layout title="Signup page" description="Signup to Node React App"
-    className="container col-md-8 offset-md-2">
+    <Layout
+      title="Signup page"
+      description="Signup to Node React App"
+      className="container col-md-8 offset-md-2"
+    >
+      {showSuccess()}
+      {showError()}
       {signUpFrom()}
-      {JSON.stringify(values)}
     </Layout>
   );
 };
