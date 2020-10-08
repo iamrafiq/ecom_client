96cbd3189ad5f1e84570f25b651d885e081bf847
Your Name <you@example.com>
2020-10-09 02:04:13 +0600

    order and order status updating

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
-import { listOrders } from "./apiAdmin";
+import { listOrders, getStatusValues, updateOrderStatus } from "./apiAdmin";
 import moment from "moment";
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [statusValues, setStatusValues] = useState([]);
+
   const { user, token } = isAuthenticated();
   const loadOrders = () => {
     listOrders(user._id, token).then((data) => {
@@ -17,8 +19,19 @@ const Orders = () => {
     });
   };
 
+  const loadStatusValues = () => {
+    getStatusValues(user._id, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setStatusValues(data);
+      }
+    });
+  };
+
   useEffect(() => {
     loadOrders();
+    loadStatusValues();
   }, []);
 
   const showOrderLength = () => {
@@ -31,6 +44,42 @@ const Orders = () => {
     }
   };
 
+  const showInput = (key, value) => (
+    <div className="input-group mb-2 mr-sm-2">
+      <div className="input-group-prepend">
+        <div className="input-group-text">{key}</div>
+      </div>
+      <input type="text" className="from-control" value={value} readOnly />
+    </div>
+  );
+  const handleStatusChange = (e, orderId) => {
+    updateOrderStatus(user._id, token, orderId, e.target.value)
+    .then(data=>{
+      if (data.error){
+        console.log("status update falied")
+      }else{
+        console.log("status update success")
+
+        loadOrders();
+      }
+    })
+  };
+  const showStatus = (o) => (
+    <div className="form-group">
+      <h3 className="mark mb-4">Status:{o.status}</h3>
+      <select
+        className="form-control"
+        onChange={(e) => handleStatusChange(e, o._id)}
+      >
+        <option>Show update status</option>
+        {statusValues.map((status, index) => (
+          <option value={status} key={index}>
+            {status}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
   return (
     <Layout
       title="Orders"
@@ -51,7 +100,7 @@ const Orders = () => {
                   <span className="bg-primary">Order Id: {o._id}</span>
                 </h2>
                 <ul className="list-group mb-2">
-                  <li className="list-group-item">{o.status}</li>
+                  <li className="list-group-item">{showStatus(o)}</li>
                   <li className="list-group-item">{o.transaction_id}</li>
                   <li className="list-group-item">{o.amount}</li>
                   <li className="list-group-item">Ordered by: {o.user.name}</li>
@@ -63,9 +112,20 @@ const Orders = () => {
                   </li>
                 </ul>
                 <h3 className="mt-4 mb-4 font-italic">
-                    Total products in the order {o.products.length}
+                  Total products in the order {o.products.length}
                 </h3>
-
+                {o.products.map((p, pIndex) => (
+                  <div
+                    className="mb-4"
+                    key={pIndex}
+                    style={{ padding: "20px", border: "1px solid indigo" }}
+                  >
+                    {showInput("Product name", p.name)}
+                    {showInput("Product price", p.price)}
+                    {showInput("Product total", p.count)}
+                    {showInput("Product Id", p._id)}
+                  </div>
+                ))}
               </div>
             );
           })}
--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -60,3 +60,33 @@ export const createCategory = (userId, token, category) => {
     })
     .catch(err => console.log(err))
 }
+
+export const getStatusValues = (userId, token) => {
+  return fetch(`${API}/order/status-values/${userId}`, {
+      method:"GET",
+      headers: {
+        Accept: "application/json",
+        Authorization:`Bearer ${token}`
+      },
+  })
+  .then(responce => {
+      return responce.json()
+  })
+  .catch(err => console.log(err))
+}
+
+export const updateOrderStatus = (userId, token, orderId, status) => {
+  return fetch(`${API}/order/${orderId}/status/${userId}`, {
+      method:"PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type":'application/json',
+        Authorization:`Bearer ${token}`
+      },
+      body: JSON.stringify({status, orderId})
+  })
+  .then(responce => {
+      return responce.json()
+  })
+  .catch(err => console.log(err))
+}
