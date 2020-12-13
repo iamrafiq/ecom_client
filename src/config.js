91cc8dfe18381564688992d08501b62ba735ef75
Your Name <you@example.com>
2020-12-13 18:00:17 +0600

    footer

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const API = process.env.REACT_APP_API_URL;
+
 export const MOBIEL_DEVICE_RESOLUTION = 700;
 export const TAB_DEVICE_RESOLUTION = 1000;
 export const SIDE_BAR_WIDTH = 15; //rem
@@ -9,6 +10,7 @@ export const APP_BAR_HEIGHT_MOBILE_SCREEN = 5; // REM
 export const SECREAT = "F$h#4";
 export const CONTACT_PHONE_NUMBER = "+8801768567184"
 export const LAN_IP = "192.168.0.106";
+
 
 
 
--- src/util/ImageUrlConverter.js
+++ src/util/ImageUrlConverter.js
@@ -2,13 +2,13 @@ import React from "react";
 import { LAN_IP } from "../config";
 
 export function imageUrlConverter(url) {
-    // console.log("url........", url)
-    if (process.env.BUILD_TYPE === "dev"){
+    console.log("build mode...", process.env.REACT_APP_BUILD_TYPE)
+    if (process.env.REACT_APP_BUILD_TYPE==="dev"){
         let rep = "";
         if (url){
-         rep = url.replace("ubuntu-Inspiron-5593:8000", LAN_IP+":8000")
+        rep = url.replace("ubuntu-Inspiron-5593:8001", LAN_IP+":8000")
         }
-        console.log("r...", rep);
+        console.log("rep...", rep);
         return rep;
     }else{
         return url
