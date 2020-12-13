8f00b8caeb23de20fc681e8c409639eed66f6468
Your Name <you@example.com>
2020-12-13 10:47:58 +0600

    wroking with deployment

--- src/util/ImageUrlConverter.js
+++ src/util/ImageUrlConverter.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { LAN_IP } from "../config";
+import { LAN_IP, BUILD_TYPE } from "../config";
 
 export function imageUrlConverter(url) {
     // console.log("url........", url)
-    // let rep = "";
-    // if (url){
-    //  rep = url.replace("ubuntu-Inspiron-5593:8001", LAN_IP+":8000")
-    // }
-    // console.log("r...", rep);
-    // return rep;
-  return url;
+    if (BUILD_TYPE === "dev"){
+        let rep = "";
+        if (url){
+         rep = url.replace("ubuntu-Inspiron-5593:8000", LAN_IP+":8000")
+        }
+        console.log("r...", rep);
+        return rep;
+    }else{
+        return url
+    }
+   
 } 
